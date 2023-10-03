806e65fddd1e837502f9fe5047ba3939fc639c24
LarsG <lars.gunnar@jobloop.no>
2023-10-03 12:19:59 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './index.css'
+
+export function App() {
+  return (
+    <div className='flex-col min-h-screen'>
+      <header>
+        <h1>First React Project</h1>
+      </header>
+
+      <main className='grow'>
+        <h2>Main Content</h2>
+      </main>
+
+      <footer>
+        <p>Kodehode</p>
+      </footer>
+    </div>
+  )
+}
